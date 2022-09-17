823b1e6fa2413bf44a2120a5ec9d7c98a4a23a60
John Hooks <hooksie11@gmail.com>
2022-09-16 20:45:20 -0400

    Add goreleaser and version

---
+++ cmd/version.go
@@ -0,0 +1,24 @@
+/*
+Copyright © 2022 John Hooks
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var Version = "dev"
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Prints the JetDocs version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(Version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
